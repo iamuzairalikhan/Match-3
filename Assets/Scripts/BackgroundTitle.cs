62f838afbd6d54fc62c208f037be233beb7a987d
Uzair Ali Khan <47238460+imuzairali@users.noreply.github.com>
2024-11-28 21:58:50 +0500

    Initial Commit (Match 3 basic mechanics)

---
+++ Assets/Scripts/BackgroundTitle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTitle : MonoBehaviour
+{
+    public GameObject[] dots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Initialize()
+    {
+        
+    }
+}
